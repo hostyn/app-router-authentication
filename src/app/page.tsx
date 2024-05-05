416b6fa23ddff020a60f1f2aae650563cc2fc5fe
Hostyn <ruben@martinezhostyn.com>
2024-05-05 18:29:39 +0200

    feat: :rocket: Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="font-mono font-bold text-6xl">Auth demo</h1>
+    </main>
+  );
+}
